e3c935302dc87d0e517f11b8ccf570f6dc83a4b5
guth.jarkovsky.tomas@gmail.com <guth.jarkovsky.tomas@gmail.com>
2022-03-06 15:14:46 +0100

    Add project files.

---
+++ Entities.cs
@@ -0,0 +1,85 @@
+﻿using NezarkaBookstoreWeb;
+using System;
+using System.Drawing;
+
+namespace JumpingPlatformGame
+{
+
+    class Entity
+    {
+        public virtual Color Color => Color.Black;
+        public WorldPoint Location;
+
+        public virtual void Update(Time sec)
+        {
+
+        }
+
+    }
+    class MovableEntity : Entity
+    {
+        public Move Horizontal;
+        public MovableEntity()
+        {
+            Horizontal = new Move();
+        }
+        public override void Update(Time sec)
+        {
+            Physics.Move(sec, Horizontal.Speed, Horizontal.LowerBound.Meters, Horizontal.UpperBound.Meters, Location.X, false);
+        }
+    }
+
+    class MovableJumpingEntity : MovableEntity
+    {
+        public Move Vertical;
+
+        public MovableJumpingEntity()
+        {
+            Vertical = new Move();
+        }
+        public override void Update(Time sec)
+        {
+            Physics.Move(sec, Horizontal.Speed, Horizontal.LowerBound.Meters, Horizontal.UpperBound.Meters, Location.X, false);
+            Physics.Move(sec, Vertical.Speed, Vertical.LowerBound.Meters, Vertical.UpperBound.Meters, Location.Y, true);
+        }
+    }
+
+    class CustomerEntity : MovableEntity
+    {
+        int years;
+        string name;
+        public override Color Color
+        {
+            get
+            {
+                if (years == -1) return Color.Gold;
+                switch (years)
+                {
+                    case 0:
+                        return Color.Black;
+                    case 1:
+                        return Color.DarkRed;
+                    case 2:
+                        return Color.Red;
+                    case 3:
+                        return Color.IndianRed;
+                    default:
+                        return Color.OrangeRed;
+                }
+            }
+        }
+
+        public CustomerEntity(Customer other)
+        {
+            name = other.ToString();
+            if (other.DateJoined == null)
+            {
+                years = -1;
+                return;
+            }
+            TimeSpan? timespan = DateTime.Now - other.DateJoined;
+            years = (int)(timespan.Value.TotalDays) / 365;
+        }
+
+    }
+}
---
+++ MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace JumpingPlatformGame {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.worldPanel = new System.Windows.Forms.Panel();
+            this.jumpingPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.customer_listBox = new System.Windows.Forms.ListBox();
+            this.button_add_customer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // worldPanel
+            // 
+            this.worldPanel.BackColor = System.Drawing.Color.White;
+            this.worldPanel.Location = new System.Drawing.Point(12, 41);
+            this.worldPanel.Name = "worldPanel";
+            this.worldPanel.Size = new System.Drawing.Size(696, 416);
+            this.worldPanel.TabIndex = 0;
+            // 
+            // jumpingPanel
+            // 
+            this.jumpingPanel.BackColor = System.Drawing.Color.Khaki;
+            this.jumpingPanel.Location = new System.Drawing.Point(12, 463);
+            this.jumpingPanel.Name = "jumpingPanel";
+            this.jumpingPanel.Size = new System.Drawing.Size(850, 96);
+            this.jumpingPanel.TabIndex = 1;
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Enabled = true;
+            this.updateTimer.Interval = 40;
+            this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+            // 
+            // customer_listBox
+            // 
+            this.customer_listBox.FormattingEnabled = true;
+            this.customer_listBox.Location = new System.Drawing.Point(713, 41);
+            this.customer_listBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.customer_listBox.Name = "customer_listBox";
+            this.customer_listBox.Size = new System.Drawing.Size(152, 329);
+            this.customer_listBox.TabIndex = 6;
+            // 
+            // button_add_customer
+            // 
+            this.button_add_customer.Location = new System.Drawing.Point(713, 374);
+            this.button_add_customer.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button_add_customer.Name = "button_add_customer";
+            this.button_add_customer.Size = new System.Drawing.Size(151, 84);
+            this.button_add_customer.TabIndex = 7;
+            this.button_add_customer.Text = "Add Customer Boxcar";
+            this.button_add_customer.UseVisualStyleBackColor = true;
+            this.button_add_customer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 571);
+            this.Controls.Add(this.button_add_customer);
+            this.Controls.Add(this.customer_listBox);
+            this.Controls.Add(this.jumpingPanel);
+            this.Controls.Add(this.worldPanel);
+            this.Name = "MainForm";
+            this.Text = "Jumping Platform Game";
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel worldPanel;
+		private System.Windows.Forms.FlowLayoutPanel jumpingPanel;
+		private System.Windows.Forms.Timer updateTimer;
+		private System.Windows.Forms.ListBox customer_listBox;
+		private System.Windows.Forms.Button button_add_customer;
+	}
+}
+
